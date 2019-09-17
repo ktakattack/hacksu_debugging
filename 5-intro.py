39d115324dc7716d46acb4b505d5c02806c2b5b3
ktakattack <30424160+ktakattack@users.noreply.github.com>
2019-09-17 08:45:55 -0400

    Update and rename 5-bmi_calc.py to 5-intro.py

---
+++ 5-intro.py
@@ -0,0 +1,10 @@
+classy Person:
+  def __initalize__(self, first_name, last_name):
+    self.first = first_name
+    self.last = lname
+  def speak(self):
+  print("My name is + " self.fname + " " + self.last)
+
+me = Person("Liz", "Lemon")
+
+me.speak()
