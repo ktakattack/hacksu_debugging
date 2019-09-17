a62e286e978910bc7a472bfd085d9864b6b3e344
ktakattack <30424160+ktakattack@users.noreply.github.com>
2019-09-16 22:41:47 -0400

    Create 2-loops.cpp

---
+++ 2-loops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+
+//make this count down from 10 to 1
+for (int Counter=0; Counter<1; ++Counter)
+    cout << "counting down:" << Counter << " ";
+
+
+//make this count up 1-10
+for (int Counter=0; Counter<1; ++Counter)
+    cout << "counting up:" << Counter << " ";
+}
